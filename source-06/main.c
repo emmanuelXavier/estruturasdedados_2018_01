102d844198217bcea233057bbe060f096297eeec
Emmanuel Xavier <emanuelsx@hotmail.com>
2018-05-11 18:40:29 -0300

    add sources

--- source-06/main.c
+++ source-06/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct node{
 	int info;
@@ -6,19 +7,21 @@ typedef struct node{
 } TFila;
 
 //sentinela
-TFila * fila;
+TFila * fila = NULL;
 
 void primeiro(int informacao){
-	fila = malloc(sizeof(TFila));
+	fila = (TFila *) malloc(sizeof(TFila));
 	fila->info = informacao;
+	fila->next = NULL;
 }
 
 void demais(int informacao){
 	TFila * novo;
 	TFila * ultimo = fila;
 	
-	novo = malloc(sizeof(TFila));
+	novo = (TFila *) malloc(sizeof(TFila));
 	novo->info = informacao;
+	novo->next = NULL;
 	
 	//levar ate o final
 	while (ultimo->next != NULL){
@@ -28,16 +31,34 @@ void demais(int informacao){
 	ultimo->next = novo;
 }
 
+void enfileirar(int informacao){
+	if (fila == NULL)
+		primeiro(informacao);
+	else
+		demais(informacao);
+}
+
+void listar(){
+	TFila * indice;
+	printf("\n\nListando\n\n");
+	for (indice = fila; indice != NULL; indice = indice->next){
+		printf("%d ",indice->info);
+	}
+}
+
 int main(int argc, char **argv)
 {
 	int leitura;
-	scanf("%d",&leitura);
+	while(1){
+		printf("Informe o valor: ");
+		scanf("%d",&leitura);
+		if (leitura == 0)
+			break;
+			
+		enfileirar(leitura);
+	}
 	
-	primeiro(leitura);
-	
-	scanf("%d",&leitura);
-	
-	demais(leitura);
+	listar();
 	
 
 	
